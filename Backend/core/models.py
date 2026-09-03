cc55fd4cfa43ca725bb1225ac8266788a8faac44
stellarise7 <nishthajaiswal712@gmail.com>
2026-09-04 00:20:27 +0530

    Add Receipt database models

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -75,3 +75,48 @@ class SavingsGoal(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.name}"
+
+
+
+
+class Receipt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    merchant = models.CharField(max_length=150)
+    receipt_date = models.DateField()
+    gst_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    category = models.CharField(max_length=100, blank=True)
+    confidence = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.merchant} - {self.total_amount}"
+
+
+class ReceiptItem(models.Model):
+    receipt = models.ForeignKey(
+        Receipt,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    name = models.CharField(max_length=150)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
