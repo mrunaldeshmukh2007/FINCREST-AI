4da5ed4d738dbe5eba930e6fa11bb3e3b64c882e
stellarise7 <nishthajaiswal712@gmail.com>
2026-09-04 00:06:08 +0530

    Add SavingsGoal database model

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -43,7 +43,7 @@ class Transaction(models.Model):
         return f"{self.user.username} - {self.transaction_type} - {self.amount}"
 
 
-        
+
 class Budget(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=100)
@@ -54,3 +54,24 @@ class Budget(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.category} - {self.amount_limit}"
+
+
+class SavingsGoal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    saved_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    monthly_contribution = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    target_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.name}"
