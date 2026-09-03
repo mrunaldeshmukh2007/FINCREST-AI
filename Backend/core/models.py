f415b961a63e433f9546b82dac4daa44ab82ebb3
stellarise7 <nishthajaiswal712@gmail.com>
2026-09-03 23:30:41 +0530

    Add Budget database model

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -41,3 +41,16 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.transaction_type} - {self.amount}"
+
+
+        
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+    amount_limit = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.category} - {self.amount_limit}"
