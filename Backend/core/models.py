30d20c183207b202b955319fdfaa8935c9139d09
stellarise7 <nishthajaiswal712@gmail.com>
2026-09-04 00:38:31 +0530

    Add Insight database model

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -133,3 +133,16 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.title}"
+
+
+
+class Insight(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title}"
